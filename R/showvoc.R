113166650ddf3bbb547be18acd7aa0a33cbda351
Vince Carey <stvjc@channing.harvard.edu>
2005-12-08 18:56:51 +0000

    simple rendering of the ontology (assumes two levels)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/biocViews@15280 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/showvoc.R
@@ -0,0 +1,19 @@
+
+showVoc <- function(g,outfile=tempfile()) {
+ require(XML)
+ top <- adj(g, nodes(g)[1])[[1]]
+ dd <- xmlTree("a")
+ dd$addTag("body", close=FALSE)
+ for (i in 1:length(top)) {
+   dd$addTag("H2", top[i])
+   nxt <- adj(g, top[i])[[1]]
+   if (length(nxt)>0) {
+      dd$addTag("UL", close=FALSE)
+      for (j in 1:length(nxt))
+         dd$addTag("LI", nxt[j])
+      dd$closeTag()
+      }
+   }
+ dd$closeTag()
+ cat(saveXML(dd$value()),sep="\n",file=outfile)
+}
