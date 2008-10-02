8f50d27856b57f4a235f5cdd8be5a064f198244c
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2008-10-02 16:06:49 +0000

    added a couple of lines to updateVocab.sh to generate SQLite DB of the onthology tree. Also rearanged some of the terms as suggested by Vince and Robert
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/biocViews@34133 bc3139a8-67e5-0310-9ffc-ced21a209358

--- updateVocab.sh
+++ updateVocab.sh
@@ -6,6 +6,7 @@ R_EXE=R
 DOT=inst/dot/biocViewsVocab.dot
 GXL=inst/dot/biocViewsVocab.gxl
 RDA=data/biocViewsVocab.rda
+SQLITE=data/biocViewsVocab.sqlite
 
 $DOT2GXL $DOT > $GXL
 
@@ -15,6 +16,15 @@ R_SCRIPT="$R_SCRIPT; con <- file('$GXL', open='r')"
 R_SCRIPT="$R_SCRIPT; biocViewsVocab <- fromGXL(con)"
 R_SCRIPT="$R_SCRIPT; save(biocViewsVocab, compress=TRUE, file='$RDA')"
 R_SCRIPT="$R_SCRIPT; close(con)"
+R_SCRIPT="$R_SCRIPT; edges <- t(sapply(strsplit(edgeNames(biocViewsVocab), '~'), c))"
+R_SCRIPT="$R_SCRIPT; colnames(edges) <- c('edgeFrom', 'edgeTo')"
+R_SCRIPT="$R_SCRIPT; if(!require(RSQLite)) warning('DBI and RSQLite are required to dump onthology to database') else{"
+R_SCRIPT="$R_SCRIPT; m <- dbDriver('SQLite')"
+R_SCRIPT="$R_SCRIPT; con <- dbConnect(m, dbname='$SQLITE')" 
+R_SCRIPT="$R_SCRIPT; res <- dbWriteTable(con, 'biocViews', as.data.frame(edges, stringsAsFactors=FALSE), row.names=FALSE, overwrite=TRUE)"
+R_SCRIPT="$R_SCRIPT; if(!res) warning('Failed writing data to database')"
+R_SCRIPT="$R_SCRIPT; res <- dbDisconnect(con)}"
 
 echo "$R_SCRIPT" | $R_EXE --slave
+
 echo "DONE"
