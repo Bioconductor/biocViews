750498c70360c69ac042e1a99740e097d2dd3d53
Patrick Aboyoun <unknown>
2008-10-18 00:17:41 +0000

    Passing the reposFullRoot argument through writeRepositoryHtml.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/biocViews@34657 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/repository.R
+++ R/repository.R
@@ -197,13 +197,14 @@ write_VIEWS <- function(reposRootPath, fields = NULL,
 
 
 writeRepositoryHtml <- function(reposRoot, title, reposUrl="..",
-                                viewUrl="../..", link.rel=TRUE) {
+                                viewUrl="../..", reposFullRoot=reposRoot,
+                                link.rel=TRUE) {
     ## Writes package description html under reposRoot/html and an index.html
     ## file under reposRoot.
     ##
     ## Links created in the package description html will use reposUrl as
     ## prefix.
-    pkgList <- loadPackageDetails(reposRoot, reposUrl, viewUrl)
+    pkgList <- loadPackageDetails(reposRoot, reposUrl, viewUrl, reposFullRoot)
     writePackageDetailHtml(pkgList, file.path(reposRoot, "html"))
     writeRepositoryIndexHtml(pkgList, reposRoot, title, link.rel=link.rel)
 
