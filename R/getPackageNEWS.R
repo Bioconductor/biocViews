d6ce62a7f12abd304241506149e8b4315f7c91d7
Martin Morgan <mtmorgan@fhcrc.org>
2011-11-08 23:16:05 +0000

    revise getPackageNews
    - check in correct locations inst/NEWS.Rd, inst/NEWS, NEWS
    - no support for tarred source
    - allow printing to markdown
    - functions to get / print new package titles
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/biocViews@60143 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getPackageNEWS.R
+++ R/getPackageNEWS.R
@@ -12,7 +12,7 @@
 getPackageNEWS <-
     function(prevRepos="http://www.bioconductor.org/packages/2.8/bioc",
              currRepos="http://www.bioconductor.org/packages/2.9/bioc",
-             srcDir, tarred=TRUE)
+             srcDir)
 {
     prev <- available.packages(contrib.url(prevRepos, "source"))
     curr <- available.packages(contrib.url(currRepos, "source"))
@@ -25,42 +25,31 @@ getPackageNEWS <-
               setNames(rep("0.0", length(newpkgs)), newpkgs))
 
     getNews <- function(pkg, ver, srcdir) {
+        newsloc <- file.path(srcdir, pkg, c("inst", "inst", "."),
+                             c("NEWS.Rd", "NEWS", "NEWS"))
+        news <- head(newsloc[file.exists(newsloc)], 1)
+        if (0L == length(news))
+            return(NULL)
         tryCatch({
-            db <- tools:::.build_news_db(pkg, dirname(srcdir), NULL, NULL)
+            db <- 
+                if (grepl("Rd$", news))
+                    tools:::.build_news_db_from_package_NEWS_Rd(news)
+                else
+                    tools:::.news_reader_default(news)
             if (!is.null(db))
                 utils::news(Version>ver, db=db)
             else NULL
         }, error=function(...) NULL)
     }
 
-    getTarredNews <- function(pkg, ver, tarball) {
-        fl <- grep("/NEWS(.Rd)?$", untar(tarball, list=TRUE), value=TRUE)
-        if (length(fl)) {
-            tryCatch({
-                exdir <- tempfile()
-                untar(tarball, exdir=exdir)
-                getTarredNews(pkg, ver, exdir)
-            } ,error=function(...) NULL)
-        } else NULL
-    }
-
-    if (tarred) {
-        FUN <- getTarredNews
-        src <- file.path(srcDir, sprintf("%s_%s.tar.gz", names(vers), vers))
-    } else {
-        FUN <- getNews
-        src <- file.path(srcDir, names(vers))
-    }
     Filter(function(x) !is.null(x) && 0L != nrow(x),
-           Map(FUN, names(vers), vers, src))
+           Map(getNews, names(vers), vers, srcDir))
 }
 
-printNEWS <- function(dbs, destfile, overwrite=FALSE, width=68, ...)
+printNEWS <- function(dbs, destfile, overwrite=FALSE, width=68,
+                      output=c("md", "text"), ...)
 {
-    if (file.exists(destfile) && !overwrite)
-        stop(.msg("'%s' exists and overwrite=FALSE", destfile))
-    file <- file(destfile, "w+")
-    on.exit(close(file))
+    output <- match.arg(output)
     dbs <- lapply(dbs, function(db) {
         db[["Text"]] <- lapply(db[["Text"]], function(elt) {
             paste(strwrap(elt, width=options()[["width"]] - 10),
@@ -68,8 +57,50 @@ printNEWS <- function(dbs, destfile, overwrite=FALSE, width=68, ...)
         })
         db
     })
-    for (i in seq_along(dbs)) {
-        cat(sprintf("\n* %s\n\n", names(dbs)[[i]]), file=file)
-        capture.output(dbs[[i]], file=file)
+    txt <- capture.output({
+        for (i in seq_along(dbs)) {
+            cat(sprintf("\n%s\n%s\n\n", names(dbs)[[i]],
+                        paste(rep("-", nchar(names(dbs)[[i]])), collapse="")))
+            print(dbs[[i]])
+        }
+    })
+    if ("md" == output) {
+        txt <- sub("^    o  ", "-", txt)
+        txt <- sub("^\t", "  ", txt)
     }
+
+    if (!is(destfile, "connection")) {
+        if (file.exists(destfile) && !overwrite)
+            stop(.msg("'%s' exists and overwrite=FALSE", destfile))
+        file <- file(destfile, "w+")
+        on.exit(close(file))
+    } else file = destfile
+    writeLines(txt, file)
+}
+
+getNewPackageTitles <-
+    function(prevRepos="http://www.bioconductor.org/packages/2.8/bioc",
+             currRepos="http://www.bioconductor.org/packages/2.9/bioc",
+             srcDir)
+{
+    prev <- available.packages(contrib.url(prevRepos, "source"))
+    curr <- available.packages(contrib.url(currRepos, "source"))
+
+    new <- sort(setdiff(rownames(curr), rownames(prev)))
+    sapply(new, function(elt) {
+        read.dcf(file.path(srcDir, elt, "DESCRIPTION"), "Title")
+    })
+}
+
+printNewPackageTitles <- function(titles, destfile, overwrite=FALSE)
+{
+    if (!is(destfile, "connection")) {
+        if (file.exists(destfile) && !overwrite)
+            stop(.msg("'%s' exists and overwrite=FALSE", destfile))
+        file <- file(destfile, "w+")
+        on.exit(close(file))
+    } else file = destfile
+    cat(strwrap(sprintf("\n- %s: %s", names(titles), titles),
+                width=70, exdent=2),
+        file=stdout(), sep="\n")
 }
