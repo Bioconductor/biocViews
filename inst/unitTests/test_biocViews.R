19da90476b2b05f542e2d10b273b405e0a5d8414
Sonali Arora <sarora@fhcrc.org>
2014-07-17 16:42:40 +0000

    unit test update
    - adding unit test for recommendBiocViews .
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/biocViews@92595 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_biocViews.R
+++ inst/unitTests/test_biocViews.R
@@ -5,4 +5,9 @@ test_findBranchReadDot <- function()
     checkException(.findBranchReadDot(current=c("RNASeq","ChipName"), 
                                       branch="Software"))
     checkException(.findBranchReadDot(current=c("Software")))
+    
+    checkException(.findBranchReadDot(
+        current=c("GUI, DNAMethylation, MethylationArray, IlluminaChip"),
+        branch=c("Software","AnnotationData","ExperimentData")))
+    
 }
