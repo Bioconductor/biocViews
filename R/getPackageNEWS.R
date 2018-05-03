896c4f68e14dd336a251831f6f3aa97a4239c5bb
lshep <shepherl@gmail.com>
2018-05-03 08:23:31 -0400

    Update functions for release time
      - getPackageNEWS doesn't rely on local checked out copy.
        gets information from master VIEWS for specified releases.
        the news information is also taken from the information
        obtained during the build process stored on master.
      - getPackageTitles now can retrieve new or removed packages
        comparing two specified bioc git branches.  It utilizes the
        manifest files to achieve this.
      - getPackageDescriptions doesn't rely on local checkout out copy.
        retrieves information from the current devel branch of bioconductor.

--- R/getPackageNEWS.R
+++ R/getPackageNEWS.R
@@ -9,20 +9,37 @@
 ## collate package NEWS files using starting version number in
 ## prevRepos, and membership in currRepos as references. Package
 ## source tree rooted at srcDir, possibiblly as tarred files
-getPackageNEWS <-
-    function(prevRepos="https://bioconductor.org/packages/release/bioc",
-             currRepos="https://bioconductor.org/packages/devel/bioc",
-             srcDir)
-{
-    prev <- available.packages(contrib.url(prevRepos, "source"))
-    curr <- available.packages(contrib.url(currRepos, "source"))
-    prev <- prev[rownames(prev) %in% rownames(curr),]
 
+# repo:  bioc data/experiment workflows
+getPackageNEWS <- function(prevRepos="3.6",
+                           currRepos="3.7",
+                           repo=c("bioc", "data/experiment", "workflows")){
+
+    repo <- match.arg(repo)
+    URL_BASE <- "http://master.bioconductor.org/packages/"
+    VIEWS <- "%s%s/%s/VIEWS"
+
+    prevUrl <- url(sprintf(VIEWS, URL_BASE, prevRepos, repo))
+    prev <- read.dcf(prevUrl, fields=c("Package", "Version"))
+    rownames(prev) <- prev[,1]
+    close(prevUrl)
+    currUrl <- url(sprintf(VIEWS, URL_BASE, currRepos, repo))
+    curr <- read.dcf(currUrl, fields=c("Package", "Version"))
+    rownames(curr) <- curr[,1]
+    close(currUrl)
+
+    prev <- prev[rownames(prev) %in% rownames(curr),]
     newpkgs <- setdiff(rownames(curr), rownames(prev))
+
     idx <- package_version(curr[newpkgs, "Version"]) >= "0.99.0"
     newpkgs <- newpkgs[idx]
     vers <- c(sub("\\.[[:digit:]]?$", ".0", prev[,"Version"]),
               setNames(rep("0.0", length(newpkgs)), newpkgs))
+
+    temp = tempdir()
+    system(paste0("scp -r webadmin@master.bioconductor.org:/extra/www/bioc/packages/",
+                  currRepos, "/", repo, "/news ", temp))
+
 
     getNews <- function(pkg, ver, srcdir) {
         newsloc <- file.path(srcdir, pkg, c("inst", "inst", "."),
@@ -43,10 +60,11 @@ getPackageNEWS <-
     }
 
     ret <- Filter(function(x) !is.null(x) && 0L != nrow(x),
-           Map(getNews, names(vers), vers, srcDir))
+                  Map(getNews, names(vers), vers, paste0(temp, "/news")))
     nms <- names(ret)
     s <- sort(nms)
     newRet <- ret[s]
+
 }
 
 
@@ -149,18 +167,31 @@ printNEWS <- function(dbs, destfile, overwrite=FALSE, width=68,
     writeLines(txt, file)
 }
 
-getNewPackageTitles <-
-    function(prevRepos="http://www.bioconductor.org/packages/2.8/bioc",
-             currRepos="http://www.bioconductor.org/packages/2.9/bioc",
-             srcDir)
-{
-    prev <- available.packages(contrib.url(prevRepos, "source"))
-    curr <- available.packages(contrib.url(currRepos, "source"))
+# manifest:  software.txt data-experiment.txt workflows.txt
+# status:  new or removed
+getPackageTitles <- function(prevBranch="RELEASE_3_6",
+                             currBranch="master",
+                             manifest=c("software.txt", "data-experiment.txt", "workflows.txt"),
+                             status = c("new", "removed")){
 
-    new <- sort(setdiff(rownames(curr), rownames(prev)))
-    sapply(new, function(elt) {
-        read.dcf(file.path(srcDir, elt, "DESCRIPTION"), "Title")
-    })
+   manifest <- match.arg(manifest)
+   status <- match.arg(status)
+
+   GIT_ARCHIVE <-
+       "git archive --remote=ssh://git@git.bioconductor.org/admin/manifest %s %s | tar -xO"
+   prevRepo <- system(sprintf(GIT_ARCHIVE, prevBranch, manifest), intern=TRUE)
+   prevRepo <- prevRepo[-which(prevRepo=="")]
+   currRepo <- system(sprintf(GIT_ARCHIVE, currBranch, manifest), intern=TRUE)
+   currRepo <- currRepo[-which(currRepo=="")]
+
+   # switch statement
+   pkgs <- switch(status,
+                  new = setdiff(currRepo, prevRepo),
+                  removed = setdiff(prevRepo, currRepo)
+                  )
+
+   gsub(pattern = "Package: ", replacement="", pkgs)
+
 }
 
 printNewPackageTitles <- function(titles, destfile, overwrite=FALSE)
@@ -178,6 +209,7 @@ printNewPackageTitles <- function(titles, destfile, overwrite=FALSE)
 
 getPackageDescriptions <- function(pkgs, outfile, output=c("md", "text"))
 {
+
     output <- match.arg(output)
     if (output == "text")
         exdent = 4
@@ -186,13 +218,18 @@ getPackageDescriptions <- function(pkgs, outfile, output=c("md", "text"))
     plower <- tolower(pkgs)
     names(plower) <- pkgs
     pkgs <- names(sort(plower))
+
+    file <- tempfile()
+    DESC_FILE <-
+        "git archive --remote=ssh://git@git.bioconductor.org/packages/%s master DESCRIPTION|tar -xO > %s"
+
     desc = lapply(pkgs, function(pkg) {
-        d = read.dcf(file.path(pkg, "DESCRIPTION"))[,"Description"]
+        system(sprintf(DESC_FILE, pkg, file))
+        d = read.dcf(file)[,"Description"]
         paste(strwrap(sprintf("- [%s](https://bioconductor.org/packages/%s) %s",
                               pkg, pkg, d), width=70, exdent=exdent),
               collapse="\n")
     })
-
     cat(noquote(unlist(desc)), sep="\n\n", file=outfile)
     invisible(NULL)
 }
