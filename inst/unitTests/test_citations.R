99abef62145620eff5c452efc7e2e21fcc9793e3
Hervé Pagès <hpages@fredhutch.org>
2019-10-18 12:20:52 -0700

    small tweak to unit tests for extractCitations()

--- inst/unitTests/test_citations.R
+++ inst/unitTests/test_citations.R
@@ -1,5 +1,5 @@
 
-setup0 <- function()
+setup1 <- function()
 {
     t <- file.path(tempdir(), "testing")
     if (file.exists(t))
@@ -23,7 +23,7 @@ setup0 <- function()
 
 test_citation_from_description <- function() 
 {
-    setup0()
+    setup1()
     t <- file.path(tempdir(), "testing")
     unlink(file.path(t, "destdir"), recursive=TRUE)
     dir.create(file.path(t, "destdir"))
@@ -40,35 +40,35 @@ test_citation_from_description <- function()
 }
 
 
-setup1 <- function()
+setup2 <- function()
 {
     t <- file.path(tempdir(), "testing")
     if (file.exists(t))
         unlink(t, recursive=TRUE)
     dir.create(t)
     dir.create(file.path(t, "testrepos", "src", "contrib"), recursive=TRUE)
-    dir.create(file.path(t, "maketarballs", "biocViews",
+    dir.create(file.path(t, "maketarballs", "biocViews2",
         "inst"), recursive=TRUE)
     dir.create(file.path(t, "destdir"))
     file.copy(system.file("DESCRIPTION", package="biocViews"),
-        file.path(t, "maketarballs", "biocViews"))
+        file.path(t, "maketarballs", "biocViews2"))
     file.copy(system.file("unitTests",
         "CITATION-tmpl", package="biocViews"),
-        file.path(t, "maketarballs", "biocViews", "inst", "CITATION"))
+        file.path(t, "maketarballs", "biocViews2", "inst", "CITATION"))
     vers <- as.character(packageVersion("biocViews"))
     oldwd <- getwd()
     on.exit(setwd(oldwd))
     setwd(file.path(t, "maketarballs"))
     tar(file.path(t, "testrepos", "src", "contrib", 
-        paste0("biocViews_", vers, ".tar.gz")),
-        "biocViews",
+                  paste0("biocViews2_", vers, ".tar.gz")),
+        "biocViews2",
         compression="gzip"
     )
 }
 
 test_citation_from_citation <- function() 
 {
-    setup1()
+    setup2()
     t <- file.path(tempdir(), "testing")
     unlink(file.path(t, "destdir"), recursive=TRUE)
     dir.create(file.path(t, "destdir"))
@@ -76,8 +76,8 @@ test_citation_from_citation <- function()
     extractCitations(file.path(t, "testrepos"), "src/contrib",
         file.path(t, "destdir"))
     checkTrue(file.exists(file.path(t, "destdir",
-        "biocViews", "citation.html")))
+        "biocViews2", "citation.html")))
     lines <- readLines(file.path(t, "destdir",
-        "biocViews", "citation.html"))
+        "biocViews2", "citation.html"))
     checkTrue(any(grepl("Open software development", lines)))    
 }
